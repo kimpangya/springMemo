856e84a9a5263c06cbafbe0a29aa65930831e5d8
kimpangya <nam74415133@gmail.com>
2024-01-22 17:20:00 +0900

    객체 중복 생성 문제 해결!

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -2,6 +2,7 @@ package com.sparta.memo.controller;
 
 import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
+import com.sparta.memo.repository.MemoRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 import service.MemoService;
@@ -11,19 +12,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class MemoController {
-
-    private final JdbcTemplate jdbcTemplate;
+    private final MemoService memoService;
 
     //외부에서 jdbc객체 받아옴
     //jdbc템플릿 사용가능
     public MemoController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+        this.memoService=new MemoService(jdbcTemplate);
     }
 
     @PostMapping("/memos")
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
         // service에서도 jdbc쓰니까 생성자 있잖음 거기에 전달해줘
-        MemoService memoService = new MemoService(jdbcTemplate);
         // 보통 controller, service의 메소드명 일치시키는 경우 많음 편함
         //service에서 처리한 거 바로 client한테 줄 거임
         return memoService.createMemo(requestDto);
@@ -32,19 +31,16 @@ public class MemoController {
 
     @GetMapping("/memos")
     public List<MemoResponseDto> getMemos() {
-        MemoService memoService = new MemoService(jdbcTemplate);
         return memoService.getMemos();
     }
 
     @PutMapping("/memos/{id}")
     public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
-        MemoService memoService = new MemoService(jdbcTemplate);
         return memoService.updateMemo(id, requestDto);
     }
 
     @DeleteMapping("/memos/{id}")
     public Long deleteMemo(@PathVariable Long id) {
-        MemoService memoService = new MemoService(jdbcTemplate);
         return memoService.deleteMemo(id);
     }
 }
--- src/main/java/service/MemoService.java
+++ src/main/java/service/MemoService.java
@@ -16,21 +16,16 @@ import java.sql.Statement;
 import java.util.List;
 
 public class MemoService {
-
-    //외부에서 jdbc객체 받아옴
-    //jdbc템플릿 사용가능
-    private final JdbcTemplate jdbcTemplate;
-    public MemoService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    private final MemoRepository memoRepository;
+    public MemoService(JdbcTemplate jdbcTemplate){
+        this.memoRepository=new MemoRepository(jdbcTemplate);
     }
-
 
     public MemoResponseDto createMemo(MemoRequestDto requestDto) {
         // RequestDto -> Entity 이건 서비스에서 해도 됨
         Memo memo = new Memo(requestDto);
 
         //DB 저장 레포지한테 맡기기
-        MemoRepository memoRepository=new MemoRepository(jdbcTemplate);
         //저장할 메모객체 넘기고 반환값으로 메모 받음 이거 어차피 다시 responsedto로 줘야됨
         Memo saveMemo = memoRepository.save(memo);
 
@@ -42,13 +37,10 @@ public class MemoService {
 
     //DB조회
     public List<MemoResponseDto> getMemos() {
-        MemoRepository memoRepository=new MemoRepository(jdbcTemplate);
                 return memoRepository.findAll();
     }
 
     public Long updateMemo(Long id, MemoRequestDto requestDto) {
-        MemoRepository memoRepository=new MemoRepository(jdbcTemplate);
-
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = memoRepository.findById(id);
         if(memo != null) {
@@ -64,8 +56,6 @@ public class MemoService {
 
 
     public Long deleteMemo(Long id) {
-        MemoRepository memoRepository=new MemoRepository(jdbcTemplate);
-
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = memoRepository.findById(id);
         if(memo != null) {
